19aadae94fd0b24a2fc3db0c29e3639c4ed3fb1b
pradip625 <kpradeepsingh001@gmail.com>
2024-12-25 11:09:31 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//console.log("this is my first visual code")
+//console.log("congrates your first code is successful")
+
+//example 2
+const num1=10;
+const num2= 4;
+const sum = num1+num2;
+console.log(" the sum of numbers = " +sum)
+const sub = num1-num2;
+console.log(" the sum of numbers = " +sub)
+const multply = num1*num2;
+console.log(" the sum of numbers = " +multply)
+const div = num1/num2;
+console.log(" the sum of numbers = " +div)
